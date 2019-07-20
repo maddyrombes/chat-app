ec5f50bde0dfdfb62c5afcd82674c5505134785f
Madeline Rombes <madelinerombes@gmail.com>
2019-07-20 15:54:34 -0400

    init app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ChatMessage from './components/ChatMessage';
+import Signup from './components/Signup'
+import ChatApp from './components/ChatApp'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Chatkit = require("@pusher/chatkit-server");
+
+const chatkit = new Chatkit.default({
+  instanceLocator: "v1:us1:21d7c55e-f2be-4d01-80b3-91dcb6ef5135",
+  key: "3abc8624-8e2f-42b0-afc3-a1bd9db4df0d:BN5eAj2+OyZVDP8CAmvRB34hWh26zc5iTU5e/VO6vpg="
+})
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUsername: '',
+      currentId: '',
+      currentView: 'Signup'
+    }
+    this.changeView = this.changeView.bind(this);
+    this.createUser = this.createUser.bind(this);
+  }
+
+  createUser(username) {
+    chatkit.createUser({
+      id: username,
+      name: username,
+    })
+      .then((currentUser) => {
+        this.setState({
+          currentUsername: username,
+          currentId: username,
+          currentView: 'chatApp'
+        })
+      }).catch((err) => {
+        if (err.status === 400) {
+          this.setState({
+            currentUsername: username,
+            currentId: username,
+            currentView: 'chatApp'
+          })
+        } else {
+          console.log(err.status);
+        }
+      });
+  }
+
+  changeView(view) {
+    this.setState({
+      currentView: view
+    })
+  }
+
+  render() {
+    let view = '';
+    if (this.state.currentView === "ChatMessage") {
+      view = <ChatMessage changeView={this.changeView} />
+    } else if (this.state.currentView === "Signup") {
+      view = <Signup onSubmit={this.createUser} />
+    } else if (this.state.currentView === "chatApp") {
+      view = <ChatApp currentId={this.state.currentId} />
+    }
+    return (
+      <div className="App">
+        {view}
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/ChatApp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ChatApp extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className="header">Let's Talk</h2>
+      </div>
+    )
+  }
+}
+
+export default ChatApp;
